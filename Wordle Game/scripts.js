b7f675b198383aaa4b5c14d6b21d8430a8a17ee5
Jacob Fuller <Fuller.Jacob@unionps.org>
2024-09-19 10:23:27 -0500

    added javascript file

---
+++ Wordle Game/scripts.js
@@ -0,0 +1,14 @@
+document.getElementById('playGameButton').addEventListener('click', function() {
+    // Start the game logic or navigate to the game page
+    alert('Starting Game!');
+});
+
+document.getElementById('instructionsButton').addEventListener('click', function() {
+    // Show the instructions
+    alert('Instructions: Use arrow keys to move, space to jump.');
+});
+
+document.getElementById('returnButton').addEventListener('click', function() {
+    // Return action (can be to a main menu, homepage, etc.)
+    alert('Returning to main menu.');
+});
